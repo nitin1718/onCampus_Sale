86c006b7e9c679a3fa45741f876d6be54c821c91
nitin <nkcnitinchy@gmail.com>
2024-08-25 18:59:02 +0530

    made some changes

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -62,7 +62,7 @@ const Cart = () => {
               <main className="md:w-3/4">
                 <article className="border border-gray-200 bg-white shadow-sm rounded mb-5 p-3 lg:p-5">
                   {cart?.cartItems?.map((cartItem) => (
-                    <div>
+                    <div key={cartItem.name}>
                       <div className="flex flex-wrap lg:flex-row gap-5  mb-4">
                         <div className="w-full lg:w-2/5 xl:w-2/4">
                           <figure className="flex leading-5">
